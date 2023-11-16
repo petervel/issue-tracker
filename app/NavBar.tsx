6549b4251a2f47510068f6bc245a0839bec0c7a7
Peter Vel <p.vel@wearedoop.com>
2023-11-16 17:06:22 +0100

    Styled navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,10 +1,15 @@
+"use client";
+
 import Link from "next/link";
 import css from "./NavBar.module.css";
 import classNames from "classnames";
 
 import { AiFillBug } from "react-icons/ai";
+import { usePathname } from "next/navigation";
 
 const NavBar = () => {
+	const pathname = usePathname();
+	console.log(pathname);
 	const links = [
 		{
 			label: "Dashboard",
@@ -22,7 +27,13 @@ const NavBar = () => {
 			</Link>
 			<ul className={css.navItems}>
 				{links.map((link) => (
-					<li className={css.link} key={link.href}>
+					<li
+						className={classNames(
+							css.link,
+							pathname === link.href ? css.active : ""
+						)}
+						key={link.href}
+					>
 						<Link href={link.href}>{link.label}</Link>
 					</li>
 				))}
