d2159631105070912d87b74831a4389029d080a1
Peter Vel <p.vel@wearedoop.com>
2023-11-18 19:54:22 +0100

    Show issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,12 +1,44 @@
-import { Button } from "@radix-ui/themes";
+import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
+import prisma from "@/prisma/client";
+import css from "./IssuesPage.module.css";
 
-const IssuesPage = () => {
+const IssuesPage = async () => {
+	const issues = await prisma.issue.findMany();
 	return (
 		<div>
-			<Button>
-				<Link href="/issues/new">New issue</Link>
-			</Button>
+			<div className={css.newIssueButton}>
+				<Button>
+					<Link href="/issues/new">New issue</Link>
+				</Button>
+			</div>
+			<Table.Root variant="surface">
+				<Table.Header>
+					<Table.Row>
+						<Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+						<Table.ColumnHeaderCell className={css.noMobile}>
+							Status
+						</Table.ColumnHeaderCell>
+						<Table.ColumnHeaderCell className={css.noMobile}>
+							Created
+						</Table.ColumnHeaderCell>
+					</Table.Row>
+				</Table.Header>
+				<Table.Body>
+					{issues.map((issue) => (
+						<Table.Row key={issue.id}>
+							<Table.Cell>
+								{issue.title}
+								<div className={css.mobileOnly}>{issue.status}</div>
+							</Table.Cell>
+							<Table.Cell className={css.noMobile}>{issue.status}</Table.Cell>
+							<Table.Cell className={css.noMobile}>
+								{issue.createdAt.toDateString()}
+							</Table.Cell>
+						</Table.Row>
+					))}
+				</Table.Body>
+			</Table.Root>
 		</div>
 	);
 };
