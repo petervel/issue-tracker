0606568d089aec412b2275b4d8da0ff681734728
Peter Vel <p.vel@wearedoop.com>
2023-11-19 11:56:53 +0100

    Built styled Link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,9 @@
-import { Button, Table } from "@radix-ui/themes";
-import Link from "next/link";
+import { Table } from "@radix-ui/themes";
 import prisma from "@/prisma/client";
 import css from "./IssuesPage.module.css";
 import IssueStatusBadge from "../components/IssueStatusBadge";
-import delay from "delay";
 import IssueActions from "./IssueActions";
+import Link from "../components/Link";
 
 const IssuesPage = async () => {
 	const issues = await prisma.issue.findMany();
