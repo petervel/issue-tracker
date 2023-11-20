20fdfa36413d36ba00c3a552eb1c52f73f9621fd
Peter Vel <p.vel@wearedoop.com>
2023-11-20 23:01:36 +0100

    Secured app

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -4,12 +4,16 @@ import { notFound } from "next/navigation";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
 import DeleteIssueButton from "./DeleteIssueButton";
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/auth/authOptions";
 
 type IssueDetailPageProps = {
 	params: { id: string };
 };
 
 const IssueDetailPage = async ({ params }: IssueDetailPageProps) => {
+	const session = await getServerSession(authOptions);
+
 	const issue = await prisma.issue.findUnique({
 		where: { id: parseInt(params.id) },
 	});
@@ -23,12 +27,14 @@ const IssueDetailPage = async ({ params }: IssueDetailPageProps) => {
 			<Box className="col-span-4">
 				<IssueDetails issue={issue} />
 			</Box>
-			<Box>
-				<Flex direction="column" gap="4">
-					<EditIssueButton issueId={issue.id} />
-					<DeleteIssueButton issueId={issue.id} />
-				</Flex>
-			</Box>
+			{session && (
+				<Box>
+					<Flex direction="column" gap="4">
+						<EditIssueButton issueId={issue.id} />
+						<DeleteIssueButton issueId={issue.id} />
+					</Flex>
+				</Box>
+			)}
 		</Grid>
 	);
 };
