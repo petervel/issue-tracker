47a7f7344577e50f6c8c494e78a118b3ac76cf60
Peter Vel <p.vel@wearedoop.com>
2023-11-21 12:10:13 +0100

    Improved API to assign issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,5 @@
 import authOptions from "@/app/auth/authOptions";
-import { issueSchema } from "@/app/validationSchemas";
+import { issueSchema, patchIssueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
@@ -15,9 +15,20 @@ export async function PATCH(
 	}
 
 	const body = await request.json();
-	const validation = issueSchema.safeParse(body);
+	const validation = patchIssueSchema.safeParse(body);
 	if (!validation.success) {
 		return NextResponse.json(validation.error.format(), { status: 400 });
+	}
+
+	const { title, description, assignedToUserId } = body;
+
+	if (assignedToUserId) {
+		const user = await prisma.user.findUnique({
+			where: { id: assignedToUserId },
+		});
+		if (!user) {
+			return NextResponse.json({ error: "Invalid user." }, { status: 400 });
+		}
 	}
 
 	const issue = await prisma.issue.findUnique({
@@ -31,8 +42,9 @@ export async function PATCH(
 	const updatedIssue = await prisma.issue.update({
 		where: { id: issue.id },
 		data: {
-			title: body.title,
-			description: body.description,
+			title,
+			description,
+			assignedToUserId,
 		},
 	});
 
