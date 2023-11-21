4700f84c4dab792e064dcfa027351da2102e9b2a
Peter Vel <p.vel@wearedoop.com>
2023-11-21 20:40:38 +0100

    Added metadata

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -41,4 +41,14 @@ const IssueDetailPage = async ({ params }: IssueDetailPageProps) => {
 	);
 };
 
+export async function generateMetadata({ params }: IssueDetailPageProps) {
+	const issue = await prisma.issue.findUnique({
+		where: { id: parseInt(params.id) },
+	});
+	return {
+		title: issue?.title,
+		description: "Details of issue " + issue?.id,
+	};
+}
+
 export default IssueDetailPage;
