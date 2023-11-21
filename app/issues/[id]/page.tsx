8fb037d81c1c709ce18c6f28d9fbb8e1ef35743d
Peter Vel <p.vel@wearedoop.com>
2023-11-21 23:17:51 +0100

    Optimized performance using React cache

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -7,17 +7,20 @@ import DeleteIssueButton from "./DeleteIssueButton";
 import { getServerSession } from "next-auth";
 import authOptions from "@/app/auth/authOptions";
 import AssigneeSelect from "./AssigneeSelect";
+import { cache } from "react";
 
 type IssueDetailPageProps = {
 	params: { id: string };
 };
 
+const fetchIssue = cache((issueId: number) =>
+	prisma.issue.findUnique({ where: { id: issueId } })
+);
+
 const IssueDetailPage = async ({ params }: IssueDetailPageProps) => {
 	const session = await getServerSession(authOptions);
 
-	const issue = await prisma.issue.findUnique({
-		where: { id: parseInt(params.id) },
-	});
+	const issue = await fetchIssue(parseInt(params.id));
 
 	if (!issue) {
 		notFound();
@@ -42,9 +45,7 @@ const IssueDetailPage = async ({ params }: IssueDetailPageProps) => {
 };
 
 export async function generateMetadata({ params }: IssueDetailPageProps) {
-	const issue = await prisma.issue.findUnique({
-		where: { id: parseInt(params.id) },
-	});
+	const issue = await fetchIssue(parseInt(params.id));
 	return {
 		title: issue?.title,
 		description: "Details of issue " + issue?.id,
