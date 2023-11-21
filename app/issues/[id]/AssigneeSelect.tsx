207f97bf886a9f843c8a3f4cb55974a070034144
Peter Vel <p.vel@wearedoop.com>
2023-11-21 12:17:37 +0100

    Assigning issues to users

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { User } from "@prisma/client";
+import { Issue, User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { Skeleton } from "@/app/components";
 
-const AssigneeSelect = () => {
+const UNASSIGNED_ID = "<unassigned>";
+const AssigneeSelect = ({ issue }: { issue: Issue }) => {
 	const {
 		data: users,
 		error,
@@ -23,11 +24,19 @@ const AssigneeSelect = () => {
 	if (error) return null;
 
 	return (
-		<Select.Root>
+		<Select.Root
+			defaultValue={issue.assignedToUserId || UNASSIGNED_ID}
+			onValueChange={(userId) => {
+				axios.patch(`/api/issues/${issue.id}`, {
+					assignedToUserId: userId !== UNASSIGNED_ID ? userId : null,
+				});
+			}}
+		>
 			<Select.Trigger placeholder="Assign..."></Select.Trigger>
 			<Select.Content>
 				<Select.Group>
 					<Select.Label>Suggestions</Select.Label>
+					<Select.Item value={UNASSIGNED_ID}>Unassigned</Select.Item>
 					{users?.map((user) => (
 						<Select.Item key={user.id} value={user.id}>
 							{user.name}
