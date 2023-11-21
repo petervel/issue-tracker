342d8874b3bf372ac91b9d3e682e2064c96c3777
Peter Vel <p.vel@wearedoop.com>
2023-11-21 17:22:48 +0100

    Implemented pagination

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
 	ChevronLeftIcon,
 	ChevronRightIcon,
@@ -5,6 +7,7 @@ import {
 	DoubleArrowRightIcon,
 } from "@radix-ui/react-icons";
 import { Button, Flex, Text } from "@radix-ui/themes";
+import { useRouter, useSearchParams } from "next/navigation";
 import { ReactNode } from "react";
 
 type PaginationProps = {
@@ -14,24 +17,45 @@ type PaginationProps = {
 };
 
 const Pagination = ({ itemCount, pageSize, currentPage }: PaginationProps) => {
+	const router = useRouter();
+	const searchParams = useSearchParams();
+
 	const pageCount = Math.ceil(itemCount / pageSize);
 	if (pageCount <= 1) return null;
 
+	const changePage = (page: number) => {
+		const params = new URLSearchParams(searchParams);
+		params.set("page", page.toString());
+		router.push(`?${params.toString()}`);
+	};
+
 	return (
 		<Flex align="center" gap="2">
-			<Text>
+			<Text size="2">
 				Page {currentPage} of {pageCount}
 			</Text>
-			<PaginationButton disabled={currentPage === 1}>
+			<PaginationButton
+				disabled={currentPage === 1}
+				onClick={() => changePage(1)}
+			>
 				<DoubleArrowLeftIcon />
 			</PaginationButton>
-			<PaginationButton disabled={currentPage === 1}>
+			<PaginationButton
+				disabled={currentPage === 1}
+				onClick={() => changePage(currentPage - 1)}
+			>
 				<ChevronLeftIcon />
 			</PaginationButton>
-			<PaginationButton disabled={currentPage === pageCount}>
+			<PaginationButton
+				disabled={currentPage === pageCount}
+				onClick={() => changePage(currentPage + 1)}
+			>
 				<ChevronRightIcon />
 			</PaginationButton>
-			<PaginationButton disabled={currentPage === pageCount}>
+			<PaginationButton
+				disabled={currentPage === pageCount}
+				onClick={() => changePage(pageCount)}
+			>
 				<DoubleArrowRightIcon />
 			</PaginationButton>
 		</Flex>
@@ -41,10 +65,15 @@ const Pagination = ({ itemCount, pageSize, currentPage }: PaginationProps) => {
 type PaginationButtonProps = {
 	disabled: boolean;
 	children: ReactNode;
+	onClick: () => void;
 };
 
-const PaginationButton = ({ disabled, children }: PaginationButtonProps) => (
-	<Button color="gray" variant="soft" disabled={disabled}>
+const PaginationButton = ({
+	disabled,
+	children,
+	onClick,
+}: PaginationButtonProps) => (
+	<Button color="gray" variant="soft" disabled={disabled} onClick={onClick}>
 		{children}
 	</Button>
 );
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Pagination from "./components/Pagination";
 
-export default function Home() {
-	return <Pagination itemCount={100} pageSize={10} currentPage={10} />;
+export default function Home({
+	searchParams,
+}: {
+	searchParams: { page: string };
+}) {
+	const page = parseInt(searchParams.page);
+	return <Pagination itemCount={100} pageSize={10} currentPage={page} />;
 }
