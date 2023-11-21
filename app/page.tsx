43703aa37b952c04ce60436677f59224de51aa68
Peter Vel <p.vel@wearedoop.com>
2023-11-21 17:58:00 +0100

    Built issue summary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
-import Pagination from "./components/Pagination";
+import IssueSummary from "./IssueSummary";
+import LatestIssues from "./LatestIssues";
+import prisma from "@/prisma/client";
 
-export default function Home({
-	searchParams,
-}: {
-	searchParams: { page: string };
-}) {
-	const page = parseInt(searchParams.page);
-	return <Pagination itemCount={100} pageSize={10} currentPage={page} />;
+export default async function Home() {
+	const open = await prisma.issue.count({ where: { status: "OPEN" } });
+	const inProgress = await prisma.issue.count({
+		where: { status: "IN_PROGRESS" },
+	});
+	const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+	return (
+		<>
+			<LatestIssues />
+			<IssueSummary open={open} inProgress={inProgress} closed={closed} />
+		</>
+	);
 }
