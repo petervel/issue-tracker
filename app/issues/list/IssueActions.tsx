0c666778ac8e50a80014dc8346cf6d46aa168021
Peter Vel <p.vel@wearedoop.com>
2023-11-21 13:36:21 +0100

    Built filter component

--- app/issues/list/IssueActions.tsx
+++ app/issues/list/IssueActions.tsx
@@ -1,14 +1,15 @@
-import { Button } from "@radix-ui/themes";
+import { Button, Flex } from "@radix-ui/themes";
 import Link from "next/link";
-import css from "./IssuesPage.module.css";
+import IssueStatusFilter from "./IssueStatusFilter";
 
 const IssueActions = () => {
 	return (
-		<div className={css.newIssueButton}>
+		<Flex mb="5" justify="between">
+			<IssueStatusFilter />
 			<Button>
 				<Link href="/issues/new">New issue</Link>
 			</Button>
-		</div>
+		</Flex>
 	);
 };
 
